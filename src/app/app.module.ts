43fbe7f4f4923ce423afa3d29868d64805c1baa2
cantekfe <cantek.frontend@gmail.com>
2019-05-15 15:43:19 +0300

    scripts degisti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {MapComponent} from './map/map.component';
-import {LocationProviderService}  from './services/location-provider.service'
 import {MatCardModule, MatListModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import{MapModule} from '../../projects/c-map-dealers/src/lib/map.module'
 @NgModule({
-  imports: [ BrowserModule, FormsModule, MatCardModule,MatListModule,FlexLayoutModule  ],
+  imports: [ BrowserModule, FormsModule, MatCardModule,MatListModule,FlexLayoutModule ,MapModule ],
   declarations: [ 
     AppComponent,
-    MapComponent,
-     ],
-  providers:[LocationProviderService],   
+     ],  
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
